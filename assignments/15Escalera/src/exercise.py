dc2e60ddb2fc7f938493b2cbdd32509a9e58b40e
Diego0103 <A01254073@itesm.mx>
2021-08-17 23:23:21 +0000

    Ejercicio 15Escalera

--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,6 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    h = float(input("Altura de la casa: "))
+    angulo = int(input("Angulo en grados: "))
+
+    angulo_radianes = math.radians(angulo)
+    largo = round(h/math.sin(angulo_radianes))
+    print("Largo de la escalera: "+str(largo))
 
 if __name__ == '__main__':
     main()
